accaff9f7642295df922b5e90aba06a7fdd0d534
colabribble <kmw5691@naver.com>
2024-12-23 17:46:14 +0900

    refactor: Swanda로 이름 변경

--- swanda/manage.py
+++ swanda/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sanda.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swanda.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- swanda/swanda/wsgi.py
+++ swanda/swanda/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for sanda project.
+WSGI config for swanda project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sanda.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swanda.settings')
 
 application = get_wsgi_application()
